2af29a86d39f78281fcf4a41eb290f8eab857fb5
USER <USER@DESKTOP-MUG33TE>
2023-01-24 13:01:03 +0530

    dsa

--- src/test/java/com/sample/page/CreatePage.java
+++ src/test/java/com/sample/page/CreatePage.java
@@ -3,7 +3,9 @@ package com.sample.page;
 import org.testng.annotations.Test;
 
 public class CreatePage {
-
+	
+	
+/////jndjsaj
 	@Test
 	public void create() {
 		System.out.println("Page has been created successfully");
